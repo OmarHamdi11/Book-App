7f6dbfc1d770429ed7039f5a6359721205bb3024
OmarHamd11 <omarellafy1@gmail.com>
2024-07-20 12:07:34 +0300

    best seller 1

--- lib/features/home/presenation/views/widgets/home_view_body.dart
+++ lib/features/home/presenation/views/widgets/home_view_body.dart
@@ -3,6 +3,7 @@ import 'package:bookly/core/utils/styles.dart';
 import 'package:bookly/features/home/presenation/views/widgets/custom_app_bar.dart';
 import 'package:bookly/features/home/presenation/views/widgets/featured_books_list_view.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class HomeViewBody extends StatelessWidget {
   const HomeViewBody({super.key});
@@ -10,7 +11,7 @@ class HomeViewBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const Padding(
-      padding: EdgeInsets.symmetric(horizontal: 24.0),
+      padding: EdgeInsets.symmetric(horizontal: 30.0),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
@@ -22,6 +23,9 @@ class HomeViewBody extends StatelessWidget {
           Text(
             'Best Seller',
             style: Styles.textStyle18,
+          ),
+          SizedBox(
+            height: 20,
           ),
           BestSellerListViewItem(),
         ],
@@ -53,8 +57,21 @@ class BestSellerListViewItem extends StatelessWidget {
               ),
             ),
           ),
+          const SizedBox(
+            width: 30,
+          ),
           Column(
-            children: [],
+            children: [
+              SizedBox(
+                width: MediaQuery.of(context).size.width * .5,
+                child: const Text(
+                  'Harry Potter and the Goblet of Fire',
+                  maxLines: 2,
+                  overflow: TextOverflow.ellipsis,
+                  style: Styles.textStyle20,
+                ),
+              )
+            ],
           )
         ],
       ),
