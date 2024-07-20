58dedc6d9d305f8fe8d54527f33e9ece0a6d8e4d
OmarHamd11 <omarellafy1@gmail.com>
2024-07-20 09:26:15 +0300

    create custom list view item

--- lib/core/utils/assets.dart
+++ lib/core/utils/assets.dart
@@ -1,3 +1,5 @@
 class AssetsData {
   static const logo = 'assets/images/Logo.png';
+  static const testImage =
+      'https://i5.walmartimages.com/seo/Harry-Potter-the-Prisoner-of-Azkaban-DVD_8d7cb6fa-cec5-457f-a8cb-f300e1ae229a.2b10ceaf2a6f357650af95c875d0c483.jpeg';
 }
