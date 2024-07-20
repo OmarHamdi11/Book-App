534794e2f9d5aab30b62a601985597f37dd69560
OmarHamd11 <omarellafy1@gmail.com>
2024-07-21 01:30:26 +0300

    add title and subtitle to book details view

--- lib/features/home/presenation/views/widgets/book_details_view_body.dart
+++ lib/features/home/presenation/views/widgets/book_details_view_body.dart
@@ -1,3 +1,4 @@
+import 'package:bookly/core/utils/styles.dart';
 import 'package:bookly/features/home/presenation/views/widgets/custom_book_details_app_bar.dart';
 import 'package:bookly/features/home/presenation/views/widgets/custom_book_image.dart';
 import 'package:flutter/material.dart';
@@ -13,8 +14,28 @@ class BookDetailsViewBody extends StatelessWidget {
         const CustomBookDetailsAppBar(),
         Padding(
           padding: EdgeInsets.symmetric(
-              horizontal: MediaQuery.of(context).size.width * .18),
+              horizontal: MediaQuery.of(context).size.width * .25),
           child: const CustomBookImage(),
+        ),
+        const SizedBox(
+          height: 43,
+        ),
+        const Text(
+          'The Jungle Book',
+          style: Styles.textStyle30,
+        ),
+        const SizedBox(
+          height: 6,
+        ),
+        Opacity(
+          opacity: .7,
+          child: Text(
+            'Rudyard Kipling',
+            style: Styles.textStyle18.copyWith(
+              fontWeight: FontWeight.w500,
+              fontStyle: FontStyle.italic,
+            ),
+          ),
         ),
       ],
     );
