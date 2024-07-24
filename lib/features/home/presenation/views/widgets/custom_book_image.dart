74d7d208158c398f974592fc2e79719bd833da70
OmarHamd11 <omarellafy1@gmail.com>
2024-07-24 09:51:00 +0300

    cached network image

--- lib/features/home/presenation/views/widgets/custom_book_image.dart
+++ lib/features/home/presenation/views/widgets/custom_book_image.dart
@@ -1,3 +1,4 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 
 class CustomBookImage extends StatelessWidget {
@@ -7,16 +8,10 @@ class CustomBookImage extends StatelessWidget {
   Widget build(BuildContext context) {
     return AspectRatio(
       aspectRatio: 2.6 / 4,
-      child: Container(
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(16),
-          image: DecorationImage(
-            fit: BoxFit.fill,
-            image: NetworkImage(
-              imageUrl ?? 'https://via.placeholder.com/300x450?text=Book+',
-            ),
-          ),
-        ),
+      child: CachedNetworkImage(
+        fit: BoxFit.fill,
+        imageUrl: imageUrl ?? 'https://via.placeholder.com/300x450?text=Book+',
+        errorWidget: (context, url, error) => const Icon(Icons.error),
       ),
     );
   }
