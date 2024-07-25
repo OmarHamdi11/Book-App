a4ac869764dee21b55dde6c9bf358a8f0f62827f
OmarHamd11 <omarellafy1@gmail.com>
2024-07-25 08:48:40 +0300

    book details view done

--- lib/features/home/presenation/views/widgets/book_action.dart
+++ lib/features/home/presenation/views/widgets/book_action.dart
@@ -1,21 +1,24 @@
+import 'package:bookly/core/utils/Function/launch_url.dart';
 import 'package:bookly/core/widgets/custom_button.dart';
+import 'package:bookly/features/home/data/models/book_model/book_model.dart';
 import 'package:flutter/material.dart';
 
 class BookAction extends StatelessWidget {
-  const BookAction({super.key});
-
+  const BookAction({super.key, required this.bookModel});
+  final BookModel bookModel;
   @override
   Widget build(BuildContext context) {
-    return const Padding(
-      padding: EdgeInsets.symmetric(horizontal: 8.0),
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 8.0),
       child: Row(
         children: [
           Expanded(
             child: CustomButton(
+              onPressed: () {},
               text: 'Free',
               backGroundColor: Colors.white,
               textColor: Colors.black,
-              borderRadius: BorderRadius.only(
+              borderRadius: const BorderRadius.only(
                 topLeft: Radius.circular(16),
                 bottomLeft: Radius.circular(16),
               ),
@@ -23,11 +26,14 @@ class BookAction extends StatelessWidget {
           ),
           Expanded(
             child: CustomButton(
+              onPressed: () async {
+                launchCustomUrl(context, 'bookModel.volumeInfo.previewLink');
+              },
               fontSize: 16,
-              text: 'preview',
-              backGroundColor: Color(0xffEF8262),
+              text: getText(bookModel),
+              backGroundColor: const Color(0xffEF8262),
               textColor: Colors.white,
-              borderRadius: BorderRadius.only(
+              borderRadius: const BorderRadius.only(
                 topRight: Radius.circular(16),
                 bottomRight: Radius.circular(16),
               ),
@@ -36,5 +42,13 @@ class BookAction extends StatelessWidget {
         ],
       ),
     );
+  }
+
+  String getText(BookModel bookModel) {
+    if (bookModel.volumeInfo.previewLink == null) {
+      return 'Not Available';
+    } else {
+      return 'Preview';
+    }
   }
 }
